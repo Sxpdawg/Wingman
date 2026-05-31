12fc62d2cd5a83e355fbe791ecf0e9182facd0df
Katlego <Katlegotsikoe@proton.me>
2026-05-31 23:03:36 +0200

    Flatten structure and set up initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Wingman App
+
+This is a Kotlin-based Android application.
+
+## Prerequisites
+- Android SDK
+- JDK 21+
+
+## Building
+Use the Gradle wrapper to build the project:
+```bash
+./gradlew build
+```
---
+++ build.gradle.kts
@@ -0,0 +1,45 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.wingman.app"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.wingman.app"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    sourceSets {
+        getByName("main") {
+            java.srcDirs(".")
+            res.srcDirs(".")
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
+}
